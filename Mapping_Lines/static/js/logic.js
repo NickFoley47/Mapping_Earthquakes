7eb2bf0c2d95e9eeed56d556569ab9e2faa20a90
Nick Foley <90662291+NickFoley47@users.noreply.github.com>
2022-07-11 19:27:17 -0500

    adding files

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -5,24 +5,18 @@ console.log("working");
 The mapid will reference the id tag in our <div> element on the index.html file.
 The setView() method sets the view of the map with a geographical center, where the first coordinate is latitude (40.7) and the second is longitude (-94.5). We set the zoom level of "4" on a scale 0–18.*/
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([39.82, -98.58], 4.5);
+let map = L.map('mapid').setView([36.1733, -120.1794], 7);
 
-// Get data from cities.js
-let cityData = cities;
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790]
+];
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000,
-        color: 'orange',
-        fillColor: "#CB8427",
-        weight: 4
-    })
-    //Let's format the population with a thousands separator by using the toLocaleString() method on the city.population in the bindPopup() method, like this:
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "red"
+}).addTo(map);
 
 
 // We create the tile layer that will be the background of our map.
