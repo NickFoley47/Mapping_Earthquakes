cc126b1d132f218546de113a10f23d08e556eb30
Nick Foley <90662291+NickFoley47@users.noreply.github.com>
2022-07-13 20:51:14 -0500

    added files

--- Mapping_GEOJSON_Points/static/js/logic.js
+++ Mapping_GEOJSON_Points/static/js/logic.js
@@ -27,9 +27,25 @@ let sanFranAirport =
             "coordinates":[-122.375,37.61899948120117]}}
 ]};
 
-// Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport).addTo(map);
 
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+      console.log(feature);
+      return L.marker(latlng)
+      .bindPopup("<h2> Airport Code: " + feature.properties.faa + "</h2> <hr> <h3> Airport Name: " + feature.properties.name + "</h3>" )
+    }
+
+  }).addTo(map);
+
+/*Grabbing our GeoJSON data.
+  L.geoJSON(sanFranAirport, {
+    onEachFeature: function(feature, layer) {
+        console.log(layer)
+        layer.bindPopup();
+     }
+}).addTo(map);*/
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
