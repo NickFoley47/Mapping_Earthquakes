11078196c998c928fe82432944a4855e2598094e
Nick Foley <90662291+NickFoley47@users.noreply.github.com>
2022-07-14 18:27:04 -0500

    added files

--- Mapping_Multiple_GEOJSON_Linestrings/static/js/logic.js
+++ Mapping_Multiple_GEOJSON_Linestrings/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -17,38 +17,30 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
+  Light: light,
   Dark: dark
 };
 
 let map = L.map('mapid', {
-  center: [30, 30],
-  zoom: 2,
-  layers: [streets]
+  center: [44.0, -80.0],
+  zoom: 3,
+  layers: [light]
 })
+
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/NickFoley47/Mapping_Earthquakes/Mapping_Multiple_GeoJSON_Points/majorAirports.json";
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/NickFoley47/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Mapping_Multiple_GEOJSON_Linestrings/torontoRoutes.json";
 
-/*Next, we'll add the d3.json() method, which returns a promise with the then() method and the anonymous function().
-Inside the d3.json() method we'll add the airportData variable.
-Inside the anonymous function() we'll add the data parameter, which references the airportData.
-We'll pass this data to the L.geoJSON() layer and then it'll be added to the map with addTo(map).*/
+
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
-// Creating a GeoJSON layer with the retrieved data
-
-  L.geoJSON(data, {
-  // We turn each feature into a marker on the map.
-    pointToLayer: function(feature, layers) {
-      console.log(feature);
-      return L.marker(layers)
-      .bindPopup("<h2> Airport Code: " + feature.properties.faa + "</h2> <hr> <h3> Airport Name: " + feature.properties.name + "</h3>" )
-    }}).addTo(map);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
 });
+
 
 
