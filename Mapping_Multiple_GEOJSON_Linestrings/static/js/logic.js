93f5ef24b912d3ffdd35ab5bf735d7d744af2b4a
Nick Foley <90662291+NickFoley47@users.noreply.github.com>
2022-07-14 21:22:07 -0500

    added files

--- Mapping_Multiple_GEOJSON_Linestrings/static/js/logic.js
+++ Mapping_Multiple_GEOJSON_Linestrings/static/js/logic.js
@@ -24,7 +24,7 @@ let baseMaps = {
 let map = L.map('mapid', {
   center: [44.0, -80.0],
   zoom: 3,
-  layers: [light]
+  layers: [dark]
 })
 
 
@@ -34,13 +34,25 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the Toronto airline routes GeoJSON URL.
 let torontoData = "https://raw.githubusercontent.com/NickFoley47/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Mapping_Multiple_GEOJSON_Linestrings/torontoRoutes.json";
 
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
   console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
-});
+ 
 
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+style: myStyle,
+onEachFeature: function(feature,layer) {
+  layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr> <h3> Destination: " + feature.properties.dst + "</h3>");
+  }
+})
+.addTo(map);
+});
 
 
